d56f1c01e4fb285f3e7ceab239892dffdcff75c3
William Herman <williamajherman@gmail.com>
2020-08-13 15:56:24 -0400

    added styles

--- src/MainText.js
+++ src/MainText.js
@@ -3,7 +3,6 @@ import React from "react";
 export default function MainText(props){
     const {textContent} = props;
     return(
-        <p>{textContent}</p>,
-        <p>needs styles</p>
+        <p>{textContent}</p>
     )
 }
