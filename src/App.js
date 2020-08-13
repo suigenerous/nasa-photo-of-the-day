8d0665819897e27cd02738de516c92c722bba9fd
William Herman <williamajherman@gmail.com>
2020-08-13 14:46:58 -0400

    files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./App.css";
+import mainImage from './MainImage'
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <{MainImage}/>
     </div>
   );
 }
